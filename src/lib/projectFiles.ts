5cbc2b5d4796b40a9f8d9737520f202214b7a435
Vivek Jadhav <139701987+vivek1504@users.noreply.github.com>
2026-05-24 15:38:45 +0530

    Update projectFiles.ts

--- src/lib/projectFiles.ts
+++ src/lib/projectFiles.ts
@@ -166,7 +166,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>
+        Vite + React
+      </h1>
       <button onClick={() => setCount(count + 1)}>
         count is {count}
       </button>
